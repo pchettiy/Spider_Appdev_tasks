919f76573a5448c52f8323893bbd994d2bb213c9
praba1110 <praba1110@gmail.com>
2015-06-15 17:40:17 +0530

    3rd task added

---
+++ Spider-task3/app/src/main/java/com/example/praba1110/spider_task3/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.praba1110.spider_task3;
+
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Handler;
+
+
+public class MainActivity extends ActionBarActivity {
+    TextView timer;
+    TextView value;
+    long n, t;
+    String url = "http://spider.nitt.edu/~vishnu/time.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timer = (TextView) findViewById(R.id.timer);
+        value = (TextView) findViewById(R.id.value);
+        t = 10;
+        Main();
+    }
+
+    public void Main() {
+
+        new GetData().execute(url);
+
+        looper();
+
+    }
+
+    public void exit(View v) {
+
+        finish();
+        System.exit(0);
+
+    }
+
+    public void looper() {
+        new CountDownTimer(1000 * t, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer.setText(String.valueOf(millisUntilFinished / 1000));
+
+            }
+
+            @Override
+            public void onFinish() {
+                t=n;
+                Main();
+
+            }
+        }.start();
+    }
+
+    public class GetData extends AsyncTask<String, Integer, Long> {
+        @Override
+        protected Long doInBackground(String... params) {
+            long no = 0;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                try {
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+                    conn.setRequestProperty("Accept-Encoding", "identity");
+                    InputStreamReader input = new InputStreamReader(conn.getInputStream());
+                    BufferedReader in = new BufferedReader(input);
+                    String i = in.readLine();
+                    no = Long.parseLong(i);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return no;
+
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            value.setText(String.valueOf(aLong));
+            n = aLong % 10;
+        }
+    }
+}
---
+++ Spider-task3/app/src/main/java/com/example/praba1110/spider_task3/getdata.java
@@ -0,0 +1,7 @@
+package com.example.praba1110.spider_task3;
+
+/**
+ * Created by praba1110 on 5/6/15.
+ */
+public interface getdata {
+}
